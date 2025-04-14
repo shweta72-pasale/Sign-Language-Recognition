c4bf0d829c0677fae4a7b55a20337e45e879edfa
shweta72-pasale <pasaleshweta14@gmail.com>
2025-04-14 21:49:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>
-  </React.StrictMode>
-);
-
+export { default as Navbar } from './navbar/Navbar.jsx'
+export { default as Home } from './home/Home.jsx'
+export { default as Header } from './header/Header.jsx'
+export { default as Footer } from './footer/Footer.jsx'
+export { default as CTA } from './cta/CTA.jsx'
+export { default as WhatComp } from './WhatComp/WhatComp.jsx'
+export { default as Features } from './Features/Features.jsx'
+export { default as Feature } from './Features/feature/Feature.jsx'
+export { default as Working} from './Working/Working.jsx'
+export { default as Detect} from './Detect/Detect.jsx'
+export { default as Testimonials} from './Testimonials/Testimonials.jsx'
+export { default as Card} from './Testimonials/Card/Card.jsx'
+export { default as NotFound} from './NotFound/NotFound.jsx'
+export { default as Dashboard} from './dashboard/Dashboard.jsx'
+export { default as ChartComp } from './dashboard/Chart/ChartComp.jsx'
+export { default as ProgressBar } from './Detect/ProgressBar/ProgressBar.jsx'
